ad04943b6eee69abd90219aaab0796fec60f7a43
Jere Vainionpää <jere.vainionpaa@vr.fi>
2024-12-16 16:34:33 +0200

    fix(entra-id): fix token request body params

--- src/providers/microsoft-entra-id.ts
+++ src/providers/microsoft-entra-id.ts
@@ -1,16 +1,23 @@
 import { CodeChallengeMethod, OAuth2Client } from "../client.js";
+import { createOAuth2Request, sendTokenRequest } from "../request.js";
 
 import type { OAuth2Tokens } from "../oauth2.js";
 
 export class MicrosoftEntraId {
+	private clientId: string;
+	private clientSecret: string;
 	private authorizationEndpoint: string;
 	private tokenEndpoint: string;
+	private redirectURI: string | null;
 
 	private client: OAuth2Client;
 
 	constructor(tenant: string, clientId: string, clientSecret: string, redirectURI: string) {
 		this.authorizationEndpoint = `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/authorize`;
 		this.tokenEndpoint = `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/token`;
+		this.clientId = clientId;
+		this.clientSecret = clientSecret;
+		this.redirectURI = redirectURI;
 		this.client = new OAuth2Client(clientId, clientSecret, redirectURI);
 	}
 
@@ -29,11 +36,19 @@ export class MicrosoftEntraId {
 		code: string,
 		codeVerifier: string
 	): Promise<OAuth2Tokens> {
-		const tokens = await this.client.validateAuthorizationCode(
-			this.tokenEndpoint,
-			code,
-			codeVerifier
-		);
+		const body = new URLSearchParams();
+		body.set("grant_type", "authorization_code");
+		body.set("code", code);
+		body.set("client_id", this.clientId);
+		body.set("client_secret", this.clientSecret);
+		if (this.redirectURI !== null) {
+			body.set("redirect_uri", this.redirectURI);
+		}
+		if (codeVerifier !== null) {
+			body.set("code_verifier", codeVerifier);
+		}
+		const request = createOAuth2Request(this.tokenEndpoint, body);
+		const tokens = await sendTokenRequest(request);
 		return tokens;
 	}
 
